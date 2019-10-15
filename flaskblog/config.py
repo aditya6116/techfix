d32c0563b0c55d2496b3951d68edb698e00630a2
Purnanand  Kumar <purnanand.kumar@CALSOFTHQ.COM>
2019-10-15 13:37:18 +0530

    Tech blog commit

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    SECRET_KEY = 'f864f2e60cf006091a661d57378a2be9'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
